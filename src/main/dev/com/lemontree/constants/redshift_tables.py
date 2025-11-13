423812d213cd0f6d531398128bb197cab9a42a0a
Shivam Gupta <de2_tfs@lemontreehotels.com>
2025-11-13 18:13:16 +0530

    added first draft future_rob

--- src/main/dev/com/lemontree/constants/redshift_tables.py
+++ src/main/dev/com/lemontree/constants/redshift_tables.py
@@ -1,6 +1,7 @@
 # GOLD layer tables
 GOLD_DIM_DATE = "gold.dim_date"
 GOLD_FACT_RESERVATIONS = "gold.fact_rsrv"
+GOLD_DIM_SOURCE_SEGMENT = "gold.dim_source_segment"
 
 # SILVER layer tables
 SILVER_PROTEL_RESERVATIONS = "silver.protel_rsrvtns"
--- src/main/dev/com/lemontree/registry/job_classes.py
+++ src/main/dev/com/lemontree/registry/job_classes.py
@@ -1,9 +1,11 @@
 from com.lemontree.runners.local_runner import LocalJobRunner
 from com.lemontree.runners.rob import Rob
+from com.lemontree.runners.future_rob import FutureRob
 
 # Map job_name to runner class
 runners_map = {
     "local": LocalJobRunner,
     "robFromCurrentDtToYearEndRunner": Rob,
+    "robFromCurrentDtToFuture": FutureRob,
     # TODO: Add more here
 }
