f2d89251d88010b906d42d322314abfb7eea122f
Soumyajit Seal <sde1_tfs@lemontreehotels.com>
2025-11-17 15:04:07 +0530

    pace

---
+++ src/main/dev/com/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lemontree",
+    version="1.0.0",
+    packages=find_packages(include=["com", "com.*"]),
+    description="Lemontree AWS Glue Jobs",
+    author="LemonTree",
+    author_email="you@example.com",
+    python_requires="=3.9",
+)
