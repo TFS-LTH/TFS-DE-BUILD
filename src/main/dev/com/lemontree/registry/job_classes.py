f5262a74bffbd7b3b78af1eed7df07c24a7eeb05
Soumyajit Seal <sde1_tfs@lemontreehotels.com>
2025-11-17 13:14:09 +0530

    pace

--- src/main/dev/com/lemontree/registry/job_classes.py
+++ src/main/dev/com/lemontree/registry/job_classes.py
@@ -1,11 +1,13 @@
 from com.lemontree.runners.local_runner import LocalJobRunner
 from com.lemontree.runners.rob.rob_runner import Rob
 from com.lemontree.runners.sp.stored_procedure_runner import StoredProcedureTest
+from com.lemontree.runners.pace.pace_runner import PaceRunner
 
 # Map job_name to runner class
 runners_map = {
     "local": LocalJobRunner,
     "robFromCurrentDtToYearEndRunner": Rob,
-    "sp": StoredProcedureTest
+    "sp": StoredProcedureTest,
+    "pace": PaceRunner
     # TODO: Add more here
 }
