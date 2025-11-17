fb6a3a32c5e00bc9191bfed6369dd979cba3e937
Shouvik De <ade2_tfs@lemontreehotels.com>
2025-11-17 15:40:33 +0530

    job class mat daily & bulk

--- src/main/dev/com/lemontree/registry/job_classes.py
+++ src/main/dev/com/lemontree/registry/job_classes.py
@@ -3,11 +3,17 @@ from com.lemontree.runners.rob import Rob
 from com.lemontree.runners.rob_daily import RobDaily
 from com.lemontree.runners.rob_bulk import RobBulk
 
+from com.lemontree.runners.rob_runners.rob_materialized_runner.rob_materialized_daily import RobMaterializedDaily
+from com.lemontree.runners.rob_runners.rob_materialized_runner.rob_materialized_bulk import RobMaterializeBulk
+
+
 # Map job_name to runner class
 runners_map = {
     "local": LocalJobRunner,
     "robFromCurrentDtToYearEndRunner": Rob,
     "robDaily": RobDaily,
-    "robBulk": RobBulk
+    "robBulk": RobBulk,
+    "matRobDaily" : RobMaterializedDaily,
+    "matRobBulk" : RobMaterializeBulk
     # TODO: Add more here
 }
