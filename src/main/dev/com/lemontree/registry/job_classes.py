a07c8a03868a7d0d8242911d5de2b2be2897c9d0
Shivam Gupta <de2_tfs@lemontreehotels.com>
2025-11-25 12:29:26 +0530

    deleted unnecessary modules

--- src/main/dev/com/lemontree/registry/job_classes.py
+++ src/main/dev/com/lemontree/registry/job_classes.py
@@ -1,22 +1,15 @@
-from com.lemontree.runners.local_runner import LocalJobRunner
 from com.lemontree.runners.rob.rob_daily import RobDaily
 from com.lemontree.runners.rob.rob_bulk import RobBulk
 from com.lemontree.runners.mat.materialized_daily import RobMaterializedDaily
 from com.lemontree.runners.mat.materialized_bulk import RobMaterializeBulk
-from com.lemontree.runners.sp.stored_procedure_runner import StoredProcedureTest
-from com.lemontree.runners.pace.pace_runner import PaceRunner
 
 # Map job_name to runner class
 runners_map = {
-    "local": LocalJobRunner,
-    "sp": StoredProcedureTest,
 
     # TODO: Add more here
-
     "robDaily": RobDaily,
     "robBulk": RobBulk,
     "matRobDaily" : RobMaterializedDaily,
-    "matRobBulk" : RobMaterializeBulk,
-    "pace": PaceRunner,
+    "matRobBulk" : RobMaterializeBulk
 
 }
